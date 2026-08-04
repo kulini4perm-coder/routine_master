0de3da8b7b3e204b6dc88def9f739d2ce926d3df
Artem Kulinich <kulini4perm@gmail.com>
2026-08-04 15:55:39 +0500

    fix: settings

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['158.160.225.2', 'localhost', '127.0.0.1', 'web', '*']
 
 
 # Application definition
